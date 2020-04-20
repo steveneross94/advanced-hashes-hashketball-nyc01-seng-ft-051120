14a87bafb571d8255ea17b0abd7594451220cfb9
Steven Ross <ross.steven@me.com>
2020-04-20 18:43:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,10 +180,8 @@ def player_stats(player_name)
   game_hash.values.each do |team_info|
     team_info[:players].each do |player|
       if player.has_value?(player_name)
-        player.delete(:player_name)
         return player
       end
   end
 end
-
 end
