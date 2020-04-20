716eb83c20ad6af38ab94c3a364bfd00d29dacd3
Steven Ross <ross.steven@me.com>
2020-04-20 17:52:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,6 +131,7 @@ end
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
+      binding.pry
       data.each do |data_item|
           binding.pry
       end
