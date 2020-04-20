ac36aa3b43459bdbd7460281f4aa8f6d206ab189
Steven Ross <ross.steven@me.com>
2020-04-20 17:46:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 def game_hash
   {
@@ -127,7 +128,15 @@ def game_hash
 end
 
 # Write code here
-
-def num_points_scored
-  game_hash[:players][:points]
+def num_points_scored(player_name)
+  binding.pry
+  game_hash.each do |location, team_data|
+    binding.pry
+    team_data.each do |attribute, data|
+      binding.pry
+      data.each do |data_item|
+          binding.pry
+      end
+    end
+  end
 end
